0d0bef630caf66463b164daed88a8a1ff1b36e08
Armando Montero <111711434+krypt0k1@users.noreply.github.com>
2024-01-18 21:24:21 -0500

    Added prompts, functionality for other slots.

--- LinuxGenRSA2048HSM.py
+++ LinuxGenRSA2048HSM.py
@@ -1,4 +1,21 @@
-# Description: This script is used to generate a new keypair on a nShield HSM Edge device.
+# Proprietary and Confidential
+# Entrust Data Corp & nCipher Security 
+# Written by Armando Montero
+
+# **************************************************************************
+# DISCLAIMER:
+# This code is provided for example purposes only.
+# It is not intended for production use. It has not been tested for security.
+# nCipher disclaims all warranties, express or implied, including without
+# limitation, any implied warranties of fitness of this code for any
+# particular purpose. nCipher shall not be liable for any damages, however
+# caused, arising out of any use of this code.
+# **************************************************************************
+
+# All rights reserved
+
+# This script is used to generate a new keypair on a nShield HSM  devices.
+
 
 # Activate venv run : source /home/administrator/Documents/.venv/bin/activate
 # install pkcs11 module : pip install python-pkcs11
@@ -29,23 +46,41 @@ print(sys.path)
 #   - PKCS11_MODULE_PATH: the path to the pkcs11 module
 #   - PKCS11_TOKEN_LABEL: the label of the token to use
 #   - PKCS11_PIN: the pin of the token to use
+
 os.environ['PKCS11_MODULE_PATH'] = '/opt/nfast/toolkits/pkcs11/libcknfast.so'
 os.environ['CKNFAST_LOADSHARING'] = '1'
 os.environ['CKNFAST_FAKE_ACCELERATOR_LOGIN'] = '1'
 
+# Defining the pkcs11 module library
 lib = pkcs11.lib(os.environ['PKCS11_MODULE_PATH'])
+
+
 # The following environment variables are optional:
 
-os.environ['CKNFAST_OVERRIDE_SECURITY_ASSURANCE'] = 'unwrap_mech;tokenkeys'
+#os.environ['CKNFAST_OVERRIDE_SECURITY_ASSURANCE'] = 'unwrap_mech;tokenkeys'
 #os.environ['CKNFAST_DEBUG'] = '10'
 #os.environ['CKNFAST_DEBUGFILE'] = '/opt/nfast/pkcs11_debug.log'
 
 
-# Token label
-token = lib.get_token(token_label='loadshared accelerator')
+# Prompt for the PKCS11_KEY_LABEL
+PKCS11_KEY_LABEL = input("Enter the key label: ")
 
-# Key label
-PKCS11_KEY_LABEL = 'testkey'
+# Prompt for the token label
+token_label = input("Enter the token label: ")
+
+# Prompt for the PKCS11_PIN
+user_pin= input("Enter the token pin: ")
+
+# Get the token using the provided label
+token = lib.get_token(token_label=token_label)
+
+
+# Prompt for the key size
+key_size = int(input("Enter the key size (e.g., 2048): "))
+
+
+# Token label
+token = lib.get_token(token_label=token_label)
 
 # The following templates are used to generate a keypair on the HSM.
 
@@ -70,7 +105,7 @@ private_key_template = {pkcs11.Attribute.TOKEN: True,
                     
 # Open Session with HSM and Generate Key Pair
 
-with token.open(rw=True) as session:
+with token.open(rw=True,user_pin=user_pin) as session:
     
     try:
         key = session.get_key(label=PKCS11_KEY_LABEL)
@@ -80,23 +115,11 @@ with token.open(rw=True) as session:
     except pkcs11.MultipleObjectsReturned:
         sys.exit('multiple objects with label="%s" already exist' % PKCS11_KEY_LABEL)
 
-    public, private = session.generate_keypair(pkcs11.KeyType.RSA, 2048, label=PKCS11_KEY_LABEL,
+    public, private = session.generate_keypair(pkcs11.KeyType.RSA, key_size, label=PKCS11_KEY_LABEL,
                                                public_template=public_key_template,
                                                private_template=private_key_template)
 
+    # Confirmation of key creation. 
     print('public key: %s' % public)
     print('private key: %s' % private)
     
-
-    
-
-
-
-    # Export the public key
-
-    #pub_key_der = pub_key[0].export()
-    #print('public key DER: %s' % pub_key_der)
-    
-# Notes:
-#     pkcs11.Attribute.EXTRACTABLE: True yields a CKR_TEMPLATE_INCONSISTENT when applied to public_template.
-#     pkcs11.Attribute.EXTRACTABLE: False yields a CKR_TEMPLATE_INCONSISTENT when applied to private_template.
