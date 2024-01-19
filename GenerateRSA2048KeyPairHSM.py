002587f917729e53cf6c73f79b00e9247b71f2e0
Armando Montero <111711434+krypt0k1@users.noreply.github.com>
2024-01-18 20:34:44 -0500

    Update GenerateRSA2048KeyPairHSM.py
    Added prompt functionality - tested and validated.

--- GenerateRSA2048KeyPairHSM.py
+++ GenerateRSA2048KeyPairHSM.py
@@ -1,3 +1,8 @@
+# Description: This script is used to generate a new keypair on a nShield HSM devices.
+
+# Activate venv run : source /home/administrator/Documents/.venv/bin/activate
+# install pkcs11 module : pip install python-pkcs11
+
 import pkcs11
 import os
 import sys
@@ -10,6 +15,7 @@ except ImportError:
 
 # Check the directories in sys.path
 import sys
+
 print(sys.path)
 
 # If you're using a virtual environment, make sure pkcs11 is installed there
@@ -24,73 +30,90 @@ print(sys.path)
 #   - PKCS11_TOKEN_LABEL: the label of the token to use
 #   - PKCS11_PIN: the pin of the token to use
 
+os.environ['PKCS11_MODULE_PATH'] = 'C:\\Program Files\\nCipher\\nfast\\toolkits\\pkcs11\\cknfast.dll'
 os.environ['CKNFAST_LOADSHARING'] = '1'
-PKCS_MODULE_PATH = os.environ['PKCS11_MODULE_PATH'] = 'C:\Program Files\nCipher\nfast\toolkits\pkcs11\cknfast.dll'
+os.environ['CKNFAST_FAKE_ACCELERATOR_LOGIN'] = '1'
+
+# Defining the pkcs11 module library
+lib = pkcs11.lib(os.environ['PKCS11_MODULE_PATH'])
+
+
 # The following environment variables are optional:
 
-os.environ['CKNFAST_OVERRIDE_SECURITY_ASSURANCE'] = 'unwrap_mech;tokenkeys'
-os.environ['CKNFAST_DEBUG'] = '10'
-os.environ['CKNFAST_DEBUGFILE'] = 'C:\Users\\Administrator\\Desktop\PKCS11KCS11debug.txt'
-
-# Key Label 
-PKCS11_TOKEN_LABEL = 'testkey'
+#os.environ['CKNFAST_OVERRIDE_SECURITY_ASSURANCE'] = 'unwrap_mech;tokenkeys'
+#os.environ['CKNFAST_DEBUG'] = '10'
+#os.environ['CKNFAST_DEBUGFILE'] = '/opt/nfast/pkcs11_debug.log'
 
 
-# The following template is used to generate a keypair on the HSM.
+# Prompt for the PKCS11_KEY_LABEL
+PKCS11_KEY_LABEL = input("Enter the key label: ")
 
-public_template = { pkcs11.Attribute.CLASS: pkcs11.ObjectClass.PUBLIC_KEY,
-             pkcs11.Attribute.TOKEN: True,
-             pkcs11.Attribute.LABEL: PKCS11_TOKEN_LABEL,
-             pkcs11.Attribute.KEY_TYPE: pkcs11.KeyType.RSA,
-             pkcs11.Attribute.MODULUS_BITS: 2048,
-             pkcs11.Attribute.SIGN: True,
-             pkcs11.Attribute.VERIFY: True,
-             pkcs11.Attribute.ENCRYPT: True,
-             pkcs11.Attribute.DECRYPT: False,
-             pkcs11.Attribute.WRAP: True,
-             pkcs11.Attribute.UNWRAP: False,
-             pkcs11.Attribute.SENSITIVE: False,
-             pkcs11.Attribute.EXTRACTABLE: True,
-             pkcs11.Attribute.ALWAYS_AUTHENTICATE: False,
-             pkcs11.Attribute.PRIVATE: True,
-             pkcs11.Attribute.Modulus: True,
-             pkcs11.Attribute.PublicExponent: True,
-             pkcs11.Attribute.PrivateExponent: True,
-             pkcs11.Attribute.Prime1: True,
-             pkcs11.Attribute.Prime2: True,
-             pkcs11.Attribute.Exponent1: True,
-             pkcs11.Attribute.Exponent2: True,
-             pkcs11.Attribute.Coefficient: True,
-             pkcs11.Attribute.PUBLIC_EXPONENT: 65537}
-            
-private_template = {pkcs11.Attribute.CLASS: pkcs11.ObjectClass.PRIVATE_KEY,
-                    pkcs11.Attribute.TOKEN: True,
-                    pkcs11.Attribute.LABEL: PKCS11_TOKEN_LABEL,
-                    pkcs11.Attribute.KEY_TYPE: pkcs11.KeyType.RSA,
-                    pkcs11.Attribute.MODULUS_BITS: 2048,
-                    pkcs11.Attribute.SIGN: True,
-                    pkcs11.Attribute.VERIFY: True,
-                    pkcs11.Attribute.ENCRYPT: False,
-                    pkcs11.Attribute.DECRYPT: True,
-                    pkcs11.Attribute.WRAP: False,
-                    pkcs11.Attribute.UNWRAP: True,
-                    pkcs11.Attribute.SENSITIVE: True,
-                    pkcs11.Attribute.EXTRACTABLE: False,
-                    pkcs11.Attribute.ALWAYS_AUTHENTICATE: False,
-                    pkcs11.Attribute.PRIVATE: True,
-                    pkcs11.Attribute.Modulus: True,
-                    pkcs11.Attribute.PrivateExponent: True,
-                   }
+# Prompt for the token label
+token_label = input("Enter the token label: ")
 
+# Get the token using the provided label
+token = lib.get_token(token_label=token_label)
+
+
+# Prompt for the key size
+key_size = int(input("Enter the key size (e.g., 2048): "))
+
+
+# Token label
+token = lib.get_token(token_label=token_label)
+
+# The following templates are used to generate a keypair on the HSM.
+
+public_key_template = {pkcs11.Attribute.TOKEN: True,
+                       pkcs11.Attribute.PUBLIC_EXPONENT: 65537,
+                       pkcs11.Attribute.MODULUS_BITS: 2048,
+                       pkcs11.Attribute.WRAP: True,
+                       pkcs11.Attribute.VERIFY: True,
+                      # pkcs11.Attribute.EXTRACTABLE: True,
+
+                      }
+
+private_key_template = {pkcs11.Attribute.TOKEN: True,
+                        pkcs11.Attribute.PRIVATE: True,
+                        pkcs11.Attribute.SENSITIVE: True,
+                        pkcs11.Attribute.UNWRAP: True,                        
+                        pkcs11.Attribute.MODIFIABLE: True,
+                        pkcs11.Attribute.EXTRACTABLE: False,
+                                            
+}
+    
+                    
 # Open Session with HSM and Generate Key Pair
 
 with token.open(rw=True) as session:
+    
     try:
-        key = session.get_key(label=PKCS11_TOKEN_LABEL)
-        sys.exit('object with label="%s" already exists' % PKCS11_TOKEN_LABEL)
-    except pkcs11.exceptions.KeyDoesNotExist:
+        key = session.get_key(label=PKCS11_KEY_LABEL)
+        sys.exit('object with label="%s" already exists' % PKCS11_KEY_LABEL)
+    except pkcs11.exceptions.NoSuchKey:
         pass
     except pkcs11.MultipleObjectsReturned:
-        sys.exit('multiple objects with label="%s" already exist' % PKCS11_TOKEN_LABEL)
-       
-    key = session.generate_keypair(pkcs11.KeyType.RSA, 2048, public_template, private_template)
+        sys.exit('multiple objects with label="%s" already exist' % PKCS11_KEY_LABEL)
+
+    public, private = session.generate_keypair(pkcs11.KeyType.RSA, key_size, label=PKCS11_KEY_LABEL,
+                                               public_template=public_key_template,
+                                               private_template=private_key_template)
+
+    print('public key: %s' % public)
+    print('private key: %s' % private)
+    
+
+    
+
+
+
+    # Export the public key
+
+    #pub_key_der = pub_key[0].export()
+    #print('public key DER: %s' % pub_key_der)
+    
+# Notes:
+#     pkcs11.Attribute.EXTRACTABLE: True yields a CKR_TEMPLATE_INCONSISTENT when applied to public_template.
+#     pkcs11.Attribute.EXTRACTABLE: False yields a CKR_TEMPLATE_INCONSISTENT when applied to private_template.try:
+  
+
