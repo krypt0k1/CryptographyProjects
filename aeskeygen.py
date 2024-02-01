a8fe765d2518501e4e191045bca3f097661916c2
Armando Montero <111711434+krypt0k1@users.noreply.github.com>
2024-02-01 13:48:37 -0500

    Added comments, improved readibility, and added a token_find option.

--- aeskeygen.py
+++ aeskeygen.py
@@ -5,22 +5,25 @@ import pkcs11
 from pprint import pprint
 import sys
 
+# Setup Configuration
 logging.basicConfig(level=logging.INFO)  # Config needed to default output to standard output
 logger = logging.getLogger(__name__)
 
+# Define the PKCS#11 DLL path
 LIB = os.path.join(os.environ.get("NFAST_HOME", '/opt/nfast'),
                                   'toolkits', 'pkcs11', 'libcknfast.so')
+# Define argument options.
 
 GENERATE = 'generate'
 DELETE = 'delete'
+TOKEN = 'token'
+
+# Define environment variables. 
 os.environ["CKNFAST_LOADSHARING"] = '1'
 os.environ["CKNFAST_OVERRIDE_SECURITY_ASSURANCES"] = 'unwrap_mech;tokenkeys'
 os.environ["CKNFAST_FAKE_ACCELERATOR_LOGIN"] = '1'
 
-if not logger.hasHandlers():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s -%(name)s - %(levelame)s -%(message)s')  # Config needed to default output to standard output
-    logger = logging.getLogger(__name__)
-
+# Define arguments. 
 def parse_args():
     """
     Parse command line arguments for the AES key generator.
@@ -30,7 +33,7 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='Entrust Python PKCS#11 key generator.',
                                      prog='aeskeygen.py',
-                                     usage='%(prog)s [options]' and '%(prog)s -[option]--token-label "loadshared accelerator" --label "default_key_label"\n',
+                                     usage='%(prog)s [--generate]' and '%(prog)s [--find-token]--token-label "loadshared accelerator" --label "default_key_label"\n',
                                      epilog="Example: %(prog)s --generate --label 'my_key' --key-size 256 --token-label 'loadshared accelerator'\n",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                      add_help=True)
@@ -62,20 +65,27 @@ def parse_args():
                         help='token label to use',
                         required=False,
                         default='loadshared accelerator')
+   parser.add_agument('f', '--find-token',
+                      help='Looks for desired token label'
+                      'f'
     args = vars(parser.parse_args())
     return args
 
-
+# Define the main function and options 
 def main():
     args = parse_args()  # Parse the arguments
     generate = GENERATE in args and args[GENERATE]
     delete = DELETE in args and args[DELETE]
-
-
+    token_found = TOKEN in args and args[TOKEN]
+  
+# Grab arguments from input. 
     key_label = args.get('label')  # Fetch the label from arguments
     key_size = args.get('key_size')  # Fetch the key size from arguments
+    user_pin = args.get('pin') # Fetch the pin from arguments. 
 
-
+# Template key. 
+  # Change accordingly
+  # Plan to add functionality to grab boolean values within arguments to customize the CKA_ATTRIBUTES of the key. 
     template = {pkcs11.Attribute.TOKEN: True,
                 pkcs11.Attribute.PRIVATE: False,
                 pkcs11.Attribute.MODIFIABLE: True,
@@ -89,19 +99,27 @@ def main():
                 pkcs11.Attribute.SIGN: False,
                 pkcs11.Attribute.VERIFY: False}
 
+  # Initializes the library 
     lib = pkcs11.lib(LIB)
-
+# Grabs the token label from the syntax argument. 
     token = lib.get_token(token_label=args['token_label'])
-    
-    if token:
+  
+ # Functionality for token_found option          
+    if token_found:
         logger.info("Token found: %s", token)
     else:
         logger.error("No token found with label='%s'.", args['token_label'])
         return
 
+# Confirms that the token was found after calling it. 
+ if token:
+        logger.info("Token found: %s", token)
 
-    with token.open(rw=True) as session:
-        if generate:
+# Open a session with the token
+    with token.open(rw=True, user_pin=) as session:
+      
+    #  Functionality for generate option       
+         if generate:
             try:
                 key = session.generate_key(pkcs11.KeyType.AES, key_size, label=key_label,
                                            template=template)
@@ -112,18 +130,14 @@ def main():
                     'Key Size: ' + str(key_size) + ' bits',
                     'Key Label: ' + key_label,
                     'Key Type: AES']
-                
+
+              # Print the completion clause and key information. 
                 pprint(key_info[::-1])
-                  #  'Key Version:' (args['key_version']),
-                   # 'Token Label:' (args['token_label']),
-                
-               # pprint(key_info)
-                #logger.info(f'Key successfully generated: \n'
-                #           f'Label: {key_label}\n'
-                ##          f'Key Object: {key}\n')
+     
+          # Error handling          
             except Exception as e:
                 logger.error("Error generating the key: %s", str(e))
-
+      # Functionality for delete option          
         if delete:
             try:
                 key = session.get_key(label=key_label)
@@ -134,17 +148,10 @@ def main():
             except pkcs11.MultipleObjectsReturned:
                 logger.error('multiple objects with label="%s"', key_label)
 
+# Call to main function. 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
         logger.error(repr(e))
         raise e
-
-
-    try:
-        main()
-    except Exception as e:
-        logger.error(repr(e))
-        raise e
-
