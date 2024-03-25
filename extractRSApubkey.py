f65fe3a097229e82c42b66f6e64df34d2e5c4dc1
Armando Montero <111711434+krypt0k1@users.noreply.github.com>
2024-03-25 12:51:41 -0400

    Python Script to extract the RSA public key from a key pair generated in an nShield HSM.
    Improved error handling capabilities. 
    Now converts DER output onto PEM format to generate self-signed certs, can be imported to other HSM's or CA's as needed.

--- extractRSApubkey.py
+++ extractRSApubkey.py
@@ -5,8 +5,7 @@ import os
 import argparse
 import logging
 from pkcs11.util.rsa import encode_rsa_public_key
-
-
+from asn1crypto import pem, x509
 
 # Defining the logger.
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -21,7 +20,7 @@ os.environ["CKNFAST_FAKE_ACCELERATOR_LOGIN"] = '1'
 
 # Arguments parser.
 def parse_args():
-    parser = argparse.ArgumentParser(description='Entrust Python PKCS#11 Extract Public Key from HSM.',
+    parser = argparse.ArgumentParser(description='python-pcks11 | Extract Public Key from nShield HSM.',
                                      prog='rsaextractpub.py',
                                      usage='%(prog)s rsaextractpub.py [options]',
                                      epilog="Example: %(prog)s --extract --token-label 'loadshared accelerator' --label 'my_key'",
@@ -38,11 +37,11 @@ def parse_args():
                         default='default_key_label')
     parser.add_argument('-t', '--token-label',
                         help='Token label to use.',
-                        required=True,
+                        required=False,
                         default='loadshared accelerator')
     parser.add_argument('-o', '--output',
                         help='Output file name.',
-                        required=True,
+                        required=False,
                         default='public_key.txt')
     args = parser.parse_args()
     
@@ -64,7 +63,7 @@ def main():
                     public_key = session.get_key(label=args.label, object_class=pkcs11.ObjectClass.PUBLIC_KEY)
                     logger.info("Public key found: %s", public_key.label)
                     
-                    extracted_public_key = pkcs11.util.rsa.encode_rsa_public_key(public_key)
+                    extracted_public_key = pkcs11.util.rsa.encode_rsa_public_key(public_key)                  
                     logger.info("Extracting public key...")
                     logger.info("Public key extracted in DER format: %s", public_key.label + " size:" + str(public_key.key_length) + " bits" + " type:"+ str(public_key.key_type))
                     
@@ -72,14 +71,38 @@ def main():
                     # Write the public key attributes to the specified output file
                     with open(args.output, 'wb') as file:
                         file.write(extracted_public_key)
-                        logger.info(f"Public key details saved to {args.output}")
                         
                         
-                    
+                    # Write the public key to the specified output file in PEM format
+                    cert = x509.Certificate.load(extracted_public_key)  
+                    with open(args.output, 'wb') as f:
+                        logger.info(f'Writing public key to {args.output} in PEM format')
+                        der_bytes = cert.dump()
+                        pem_bytes = pem.armor('CERTIFICATE', der_bytes)
+                        f.write(pem_bytes)
+                        logger.info(f"Public key saved to {args.output} in PEM format")
+                            
+                        
+                # Error handling.     
                 except pkcs11.exceptions.NoSuchKey:
                     logger.error("No key found with label='%s'.", args.label)
+                except pkcs11.NoSuchToken:
+                    logger.error("No token found with label='%s'.", args.token_label)
+                except pkcs11.TokenNotPresent:
+                    logger.error("Token not present.")
                 except pkcs11.exceptions.FunctionFailed:
                     logger.error("Failed to extract public key.")
+                except pkcs11.FunctionCancelled:
+                    logger.error("Function cancelled.")               
+                except pkcs11.ArgumentsBad:
+                    logger.error("Bad arguments.")
+                except pkcs11.SessionHandleInvalid:
+                    logger.error("Session handle invalid.")
+                except pkcs11.SessionClosed:
+                    logger.error("Session closed.")
+                except pkcs11.ObjectHandleInvalid:
+                    logger.error("Object handle invalid.")
+                       
                 except Exception as e:
                     logger.error(f"An error occurred, review the logs: {e}")
        
